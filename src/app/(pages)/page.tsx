285a3fc5a4425053f263e6824d886c5b7f16ef83
davy1ex <dezy1ex@gmail.com>
2025-06-27 01:24:44 +0400

    Refactor: Change routes path

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/shared/ui/button";
+import { ResumeDetail } from "@/features/resume-detail/server";
+
+export default function Home() {
+  return (
+    <>
+      bla-bla-bla
+    </>
+  );
+}
