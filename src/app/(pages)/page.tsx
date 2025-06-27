2edbac1592e92f45e201da6f4611d6599338b172
davy1ex <dezy1ex@gmail.com>
2025-06-27 13:16:05 +0400

    Feature: Add Resume card for main page

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,10 +1,13 @@
+"use client"
 import { Button } from "@/shared/ui/button";
 import { ResumeDetail } from "@/features/resume-detail/server";
+import { ResumeCard } from "@/entities/resume/server";
 
 export default function Home() {
   return (
     <>
-      bla-bla-bla
+      <ResumeCard />
+
     </>
   );
 }
